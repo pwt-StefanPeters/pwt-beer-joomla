68f5cdb90c945a130c8efc7e1739844c60982d81
Stefan <stefan.peters1999@hotmail.com>
2020-02-06 16:22:23 +0100

    small update ber component

--- administrator/components/com_beers/sql/updates/mysql/0.0.11.sql
+++ administrator/components/com_beers/sql/updates/mysql/0.0.11.sql
@@ -5,7 +5,7 @@ CREATE TABLE `#__beers` (
 	`name` VARCHAR(100) NOT NULL,
 	`tagline` VARCHAR(255) NOT NULL,
 	`description` TEXT(500) NOT NULL,
-	`alcohol_percentage` DECIMAL(2,1) NOT NULL,
+	`abv` DECIMAL(2,1) NOT NULL,
 	`rating` INT(11) NULL,
     `state` tinyint(3) NOT NULL DEFAULT 0,
     `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
